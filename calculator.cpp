3daf1044d88d7907017eadbf48ff5822e16347ff
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    correct functions with new multiplication operation
    Add a member-function which performs multiplication of two float point
    digits.
    
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -2,10 +2,24 @@
 
 int Calculator::Add (double a, double b)
 {
-    return a + b + 0.5;
+    double Max;
+ 
+    if (a > b) {
+	Max = a;
+    } else {
+	Max = b;
+    }
+
+    return Max + a + b;
 }
 
 int Calculator::Sub (double a, double b)
 {
-    return Add (a, -b);
+    if (a > b) return Add(a, -b);
+    else return Add(b, -a);
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return (a + 0.5) * (b + 1);
 }
